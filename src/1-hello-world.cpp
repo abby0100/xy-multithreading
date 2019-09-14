e1986de31d873e8abd6f7675ddb42b72467eff6e
xy18 <yuan.xiong@intel.com>
2019-09-14 23:23:49 +0800

    update multithread task

--- src/1-hello-world.cpp
+++ src/1-hello-world.cpp
@@ -9,14 +9,35 @@ using namespace std;
 
 
 void hello_multithread() {
+	xy_print("Hello world, multithread.\n");
+	return;
 }
+
+class xy_task {
+public:
+	void operator()() const {
+		xy_print("Hello world, xy_task.\n");
+		return;
+	}
+};
 
 int main() {
 
-	xy_print_pid("START\n");
-	xy_print_time("%s\n", __func__);
-	xy_print_time_simple("%s\n", __func__);
+	xy_print("Hello world.\n");
+	//xy_print_simple("Hello world.\n");
 
-	thread t(hello_multithread);
-	t.join();
+	thread t1(hello_multithread);
+	t1.join();
+	
+	xy_task task;
+	thread t2(task);
+	t2.join();
+
+	thread t3{ xy_task() };
+	t3.join();
+
+	thread t4([]{
+		xy_print("Hello world, anonymous.\n");
+	});
+	t4.join();
 }
--- src/xylog.h
+++ src/xylog.h
@@ -3,29 +3,49 @@
 
 #include <unistd.h>
 
-#define xy_print_base(format, args...) {	\
-	printf(format, ##args);					\
+#define xy_print_base(format, args...) {					\
+	printf(format, ##args);									\
 }
 
-#define xy_print_time_simple(format, args...) {				\
-	time_t timep;	\
-	time(&timep);	\
-	struct tm *t = gmtime(&timep);			\
-	xy_print_base("[%02d:%02d:%02d]" format,\
-		t->tm_hour, t->tm_min, t->tm_sec, ##args)			\
-}
-
-#define xy_print_time(format, args...) {	\
-	time_t timep;	\
-	time(&timep);	\
-	struct tm *t = gmtime(&timep);			\
-	xy_print_base("[%4d-%02d-%02d %02d:%02d:%02d]" format,	\
+#define xy_print_time(format, args...) {					\
+	time_t timep;											\
+	time(&timep);											\
+	struct tm *t = gmtime(&timep);							\
+	xy_print_base("%4d-%02d-%02d %02d:%02d:%02d\t" format,	\
 		t->tm_year+1900, t->tm_mon, t->tm_mday,				\
 		t->tm_hour, t->tm_min, t->tm_sec, ##args)			\
 }
 
-#define xy_print_pid(format, args...) {		\
-	xy_print_base("[%4d]" format, getpid(), ##args)		\
+#define xy_print_time_simple(format, args...) {				\
+	time_t timep;											\
+	time(&timep);											\
+	struct tm *t = gmtime(&timep);							\
+	xy_print_base("%02d:%02d:%02d\t" format,				\
+		t->tm_hour, t->tm_min, t->tm_sec, ##args)			\
+}
+
+#define xy_print_pid(format, args...) {						\
+	xy_print_time("%4d %d\t" format,getppid(), getpid(), ##args)			\
+}
+
+#define xy_print_pid2(format, args...) {					\
+	xy_print_time_simple("%4d %4d\t" format, getppid(), getpid(), ##args)	\
+}
+
+#define xy_print_func(format, args...) {					\
+	xy_print_pid("%s: " format, __func__, ##args)			\
+}
+
+#define xy_print_func2(format, args...) {					\
+	xy_print_pid2("%s\t" format, __func__, ##args)			\
+}
+
+#define xy_print(format, args...) {							\
+	xy_print_func(format, ##args)							\
+}
+
+#define xy_print_simple(format, args...) {					\
+	xy_print_func2(format, ##args)							\
 }
 
 #endif
